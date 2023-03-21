cf96e58757ac29af584bd92db4347d20500e64ed
JawadSav <Jawadfraoui@gmail.com>
2023-03-21 06:07:18 +0100

    css

--- authentification.php
+++ authentification.php
@@ -1,6 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <form action="login.php" method="post">
-<link rel="stylesheet" href="css/connexion.css" />
+<link rel="stylesheet" href="css/Styles.css" />
+</head>
+<body>
+<div class="container">
+<div class="form-group">
 Votre login : <input type="text" name="login"><br />
+</div>
+
+<div class="form-group">
 Votre mot de passe : <input type="password" name="pwd"><br />
+
+
+
+</div>
+<div class="button">
 <input type="submit" value="Connexion">
+
+</div>
+</body>
+</html>
 </form>
